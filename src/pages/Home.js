83aecba1d55da5b1019b4e2ce07c97edc12d3a31
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2023-03-03 09:30:37 +0100

    fix btn link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import NavBar from "../components/NavBar"
+import { NavLink } from "react-router-dom"
 
 export default function Home(){
     return (
@@ -7,7 +8,8 @@ export default function Home(){
         <div className="about--div">
        <h1>Explore the weather details of your environ</h1>
        <p>Want to know the weather condition of your city? Find out with just one click.</p>
-       <a className="btn--link" href="/City">Get Started</a>
+       <NavLink className="btn--link" to="/City">Get Started</NavLink>
+       {/* <a className="btn--link" href="/City">Get Started</a> */}
        </div>
         </div>
     )

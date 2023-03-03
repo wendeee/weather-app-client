145e46ede8123780426401a4644844f72eb250bf
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2023-03-03 05:26:44 +0100

    fix bug

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ export default function Home(){
         <div className="about--div">
        <h1>Explore the weather details of your environ</h1>
        <p>Want to know the weather condition of your city? Find out with just one click.</p>
-       <a className="btn--link" href="/city">Get Started</a>
+       <a className="btn--link" href="/City">Get Started</a>
        </div>
         </div>
     )
